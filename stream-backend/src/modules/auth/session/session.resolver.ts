81458855c1d89a5208a00f4401d57f9dc8264508
PRICIDON <dddbjjk6@gmail.com>
2025-01-04 18:42:45 +0300

    Начало работы с отправкой почты

--- stream-backend/src/modules/auth/session/session.resolver.ts
+++ stream-backend/src/modules/auth/session/session.resolver.ts
@@ -3,20 +3,48 @@ import { SessionService } from "./session.service";
 import { UserModel } from "../account/models/user.module";
 import { GqlContext } from "../../../shared/types/gql-context.types";
 import { LoginInput } from "./inputs/login.input";
+import { UserAgent } from "../../../shared/decocators/user-agent.decorator";
+import { Authorization } from "../../../shared/decocators/auth.decorator";
+import { SessionModel } from "./models/session.model";
 
 @Resolver("Session")
 export class SessionResolver {
   constructor(private readonly sessionService: SessionService) {}
 
-  @Mutation(() => UserModel, { name: "loginUser" })
-  async login(@Context() { req }: GqlContext, @Args("data") input: LoginInput) {
-    return this.sessionService.login(req, input);
+  @Authorization()
+  @Query(() => [SessionModel], { name: "findSessionsByUser" })
+  async findByUser(@Context() { req }: GqlContext) {
+    return this.sessionService.findByUser(req);
   }
-  @Mutation(() => Boolean, { name: "logoutUser" })
-  async logout(
+
+  @Authorization()
+  @Query(() => [SessionModel], { name: "findCurrentSession" })
+  async findCurrent(@Context() { req }: GqlContext) {
+    return this.sessionService.findCurrentUser(req);
+  }
+
+  @Mutation(() => UserModel, { name: "loginUser" })
+  async login(
     @Context() { req }: GqlContext,
     @Args("data") input: LoginInput,
+    @UserAgent() userAgent: string,
   ) {
+    return this.sessionService.login(req, input, userAgent);
+  }
+  @Authorization()
+  @Mutation(() => Boolean, { name: "logoutUser" })
+  async logout(@Context() { req }: GqlContext) {
     return this.sessionService.logout(req);
+  }
+
+  @Authorization()
+  @Mutation(() => Boolean, { name: "removeSession" })
+  async remove(@Context() { req }: GqlContext, @Args("id") id: string) {
+    return this.sessionService.remove(req, id);
+  }
+
+  @Mutation(() => Boolean, { name: "clearSessionCookie" })
+  async clearSession(@Context() { req }: GqlContext) {
+    return this.sessionService.clearSession(req);
   }
 }
--- stream-backend/src/modules/auth/session/session.service.ts
+++ stream-backend/src/modules/auth/session/session.service.ts
@@ -1,4 +1,5 @@
 import {
+  ConflictException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -9,15 +10,57 @@ import { LoginInput } from "./inputs/login.input";
 import type { Request } from "express";
 import { verify } from "argon2";
 import { ConfigService } from "@nestjs/config";
+import { getSessionMetadata } from "../../../shared/utils/session-metadata.utils";
+import { RedisService } from "../../../core/redis/redis.service";
 
 @Injectable()
 export class SessionService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly configService: ConfigService,
+    private readonly redisService: RedisService,
   ) {}
 
-  async login(req: Request, input: LoginInput) {
+  async findByUser(req: Request) {
+    const userId = req.session.userId;
+
+    if (!userId) {
+      throw new NotFoundException("Пользователь не обнаружен в сессии");
+    }
+
+    const keys = await this.redisService.keys("*");
+    const userSessions = [];
+    for (const key of keys) {
+      const sessionData = await this.redisService.get(key);
+      if (sessionData) {
+        const session = JSON.parse(sessionData);
+        if (session.userId === userId) {
+          userSessions.push({
+            ...session,
+            id: key.split(":")[1],
+          });
+        }
+      }
+    }
+    userSessions.sort((a, b) => b.createdAt - a.createdAt);
+    return userSessions.filter((session) => session.id !== req.session.id);
+  }
+
+  async findCurrentUser(req: Request) {
+    const sessionId = req.session.userId;
+
+    const sessionData = await this.redisService.get(
+      `${this.configService.getOrThrow<string>("SESSION_FOLDER")}${sessionId}`,
+    );
+
+    const session = JSON.parse(sessionData);
+
+    return {
+      ...session,
+      id: sessionId,
+    };
+  }
+  async login(req: Request, input: LoginInput, userAgent: string) {
     const { login, password } = input;
     const user = await this.prismaService.user.findFirst({
       where: {
@@ -31,9 +74,12 @@ export class SessionService {
     if (!isValidPassword) {
       throw new UnauthorizedException("Неверный пароль");
     }
+
+    const metadata = getSessionMetadata(req, userAgent);
     return new Promise((resolve, reject) => {
       req.session.createdAt = new Date();
       req.session.userId = user.id;
+      req.session.metadata = metadata;
       req.session.save((err) => {
         if (err) {
           return reject(
@@ -58,5 +104,19 @@ export class SessionService {
         resolve(true);
       });
     });
+  }
+
+  async clearSession(req: Request) {
+    req.res.clearCookie(this.configService.getOrThrow("SESSION_NAME"));
+    return true;
+  }
+  async remove(req: Request, id: string) {
+    if (req.session.id === id) {
+      throw new ConflictException("Текущую сессию удалить нельзя");
+    }
+    await this.redisService.del(
+      `${this.configService.getOrThrow<string>("SESSION_FOLDER")}${id}`,
+    );
+    return true;
   }
 }
