c08162cc75cc2262a0a5701bc97d78e562a48859
PRICIDON <dddbjjk6@gmail.com>
2025-02-08 21:38:58 +0300

    Нихера у меня не работает, я плачу

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,15 @@
 import {authStore} from "@/store/auth/auth.store";
+import { cookies } from 'next/headers'
 
 export function useAuth() {
     const isAuthenticated = authStore(state => state.isAuthenticated)
     const setIsAuthenticated = authStore(state => state.setIsAuthenticated)
 
     const auth = () => setIsAuthenticated(true)
-    const exit = () => setIsAuthenticated(false)
+    const exit = () => {
+        cookies().then(r => r.delete('session'))
+        setIsAuthenticated(false)
+    }
 
     return {
         isAuthenticated,
