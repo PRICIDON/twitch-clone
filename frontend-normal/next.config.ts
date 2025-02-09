9fc467eb1bcdc5d1d303f3a0c5fd0ef5d4582ced
PRICIDON <dddbjjk6@gmail.com>
2025-02-09 20:34:25 +0300

    Да

---
+++ frontend-normal/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./src/libs/i18n/request.ts')
+
+const nextConfig: NextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'a78849ec-4ece-41ea-96e8-f46c3b613b8c.selstorage.ru'
+			}
+		]
+	}
+}
+
+export default withNextIntl(nextConfig)
